6903a4b9ae2984dc4761b5d8f0100374eefe0afa
miruetoto <miruetoto@gmail.com>
2025-06-13 23:48:27 +0900

    .

--- push.sh
+++ push.sh
@@ -5,9 +5,10 @@ rm index.html
 git add .
 git commit -m .
 git switch main 
-quarto publish gh-pages --no-browser --no-prompt 
+quarto publish gh-pages --no-browser --no-prompt --force
 rm -rf _site
 git add .
 git commit -m .
 git push origin main 
-git push second gh-pages
+#git push second gh-pages
+git push second gh-pages --force
